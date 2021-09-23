0b3c860256ffbad3ed48e924477fda6071b0b342
AllSafeCybercurity <skull32273227@gmail.com>
2021-09-23 12:42:50 -0700

    'wagcms'

---
+++ wagcms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-x2=gqdg=nm7@#5(%awt5^*=z)%95ue6ma34d9v8ccq*7+$i+_n'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
